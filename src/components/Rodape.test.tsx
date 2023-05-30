80fb368b1e0ed32ae901d4b9b1dc19fa3e9a5f2f
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-05-30 20:33:18 -0300

    feat: adicionando componente Rodape

---
+++ src/components/Rodape.test.tsx
@@ -0,0 +1,64 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { RecoilRoot } from "recoil";
+import Rodape from "./Rodape";
+import { useListaDeParticipantes } from "../state/hooks/useListaDeParticipantes";
+
+jest.mock('../state/hooks/useListaDeParticipantes', () => {
+   return {
+        useListaDeParticipantes: jest.fn(),
+    } 
+});
+
+const mockNavigation = jest.fn();
+
+jest.mock('react-router-dom', () => {
+    return {
+        useNavigate: () => mockNavigation
+    }
+});
+
+describe('Onde não existem participantes suficientes', () => {
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue([]);
+    });
+
+    test('A brincadeira não pode ser iniciada', () => {
+        render(<RecoilRoot>
+            <Rodape />
+        </RecoilRoot>);
+
+        const botao = screen.getByRole('button');
+
+        expect(botao).toBeDisabled();
+    });
+});
+
+describe('Onde existem 3 participantes ou mais', () => {
+    const participantes = ['Alessandro', 'Matheus', 'Joao'];
+
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+    });
+
+    test('A brincadeira não pode ser iniciada', () => {
+        render(<RecoilRoot>
+            <Rodape />
+        </RecoilRoot>);
+
+        const botao = screen.getByRole('button');
+
+        expect(botao).not.toBeDisabled()
+    });
+
+    test('A brincadeira foi iniciada', () => {
+        render(<RecoilRoot>
+            <Rodape />
+        </RecoilRoot>);
+
+        const botao = screen.getByRole('button');
+        fireEvent.click(botao);
+
+        expect(mockNavigation).toHaveBeenCalledTimes(1);
+        expect(mockNavigation).toHaveBeenCalledWith('/sorteio');
+    });
+});
