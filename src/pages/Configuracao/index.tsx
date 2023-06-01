0f134b09a84c32ad66939460dc37e589662b8660
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-06-01 20:58:25 -0300

    feat: página de Configuração

---
+++ src/pages/Configuracao/index.tsx
@@ -0,0 +1,13 @@
+import Formulario from "../../components/Formulario";
+import ListaParticipantes from "../../components/ListaParticipantes";
+import Rodape from "../../components/Rodape";
+
+export default function Configuracao() {
+    return (
+        <>
+            <Formulario />
+            <ListaParticipantes />
+            <Rodape />
+        </>
+    );
+}
